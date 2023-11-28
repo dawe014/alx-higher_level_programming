cb965fc37e64d972ccd6e2baba463cbd67ca6ed8
dawe014 <dawittamiru014@gmail.com>
2023-11-28 16:46:43 +0300

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
-for c in range(ord('a'), ord('z') + 1):
-    if chr(c) != 'e' and chr(c) != 'q':
-        print(chr(c), end='')
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
+    print("{:c}".format(i), end='')
