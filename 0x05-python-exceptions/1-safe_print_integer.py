e962d080fe1ea0d6dfc70646bae6baba1226b362
dawe014 <dawittamiru014@gmail.com>
2023-12-20 15:20:27 +0300

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return 1
-    except:
-        return 0
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    result=None
     try:
-        result = a / b
-        return result
-    except ZeroDivisionError:
-        pass
-        # return "none"
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        if result:
-            print("Inside result: {}".format(result))
-        else:
-            print("Inside result: {}".format("none"))
-        
+        print("Inside result: {}".format(div))
+    return (div)
