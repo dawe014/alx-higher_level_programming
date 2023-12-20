7a0ae906ed7935916c466a9db0d268fa3f1a13c6
dawe014 <dawittamiru014@gmail.com>
2023-12-20 15:34:55 +0300

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 class Square:
     """A class representing a square.
-
     Attributes:
         __size (int): The size of the square.
     """
-
     def __init__(self, size):
         """Initializes a new Square instance.
-
         Args:
             size (int): The size of the square.
-
         Raises:
             ValueError: If the provided size is not a positive integer.
-        """
-        if not isinstance(size, int) or size <= 0:
-            raise ValueError("Size must be a positive integer.")
-        
+        """        
         self.__size = size
