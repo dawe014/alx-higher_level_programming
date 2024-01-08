5655616725dfda609ff36d9bfdf4a86b3d0f0559
dawe014 <dawittamiru014@gmail.com>
2024-01-08 16:16:05 +0300

    update

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3 
+import numpy as np 
+"""Lazy Matrix Multiplication 
+This is our function to be tested 
+"""  
+def lazy_matrix_mul(m_a, m_b): 
+    """Multiples two matrices 
+    Args: 
+        m_a (list): first matrix 
+        m_b (list): second matrix 
+    Returns: 
+        Returns the product matrix 
+    """ 
+    try: 
+        result = np.matmul(m_a, m_b) 
+    except: 
+        raise 
+    return result
