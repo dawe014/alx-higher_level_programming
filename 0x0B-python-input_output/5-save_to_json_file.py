417e3dcab36d407850bc62e91379387b2dcd1661
dawe014 <dawittamiru014@gmail.com>
2024-01-10 00:37:24 +0300

    update

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-from json import dump
+
 """
-Save objrct to a file
+Defines a JSON file writing function.
 """
+import json
+
 def save_to_json_file(my_obj, filename):
-    """
-    Writes an object to text file using JSON repr
-    """
-    with open(filename, "w", encoding="UTF-8") as f:
-        dump(my_obj, f)
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
