bc0f98aabaa357b181c415bed2e577824c10f8bb
Dawit Tamiru <dawittamiru014@gmail.com>
2024-03-21 21:15:57 +0300

    updated

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
 """
-    script that takes in an argument and displays all values in the states table
-    of hbtn_0e_0_usa where name matches the argument
+Script that takes an argument and displays all values in the states table
+of hbtn_0e_0_usa where the name matches the argument.
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    if len(sys.argv) != 5:
+        print("Usage: ./script.py <username> <password> <database_name> <state_name>")
+        sys.exit(1)
+
+    try:
+        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        cursor = db.cursor()
+        query = "SELECT * FROM `states` WHERE BINARY `name` = %s"
+        cursor.execute(query, (sys.argv[4],))
+        for state in cursor.fetchall():
+            print(state)
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
