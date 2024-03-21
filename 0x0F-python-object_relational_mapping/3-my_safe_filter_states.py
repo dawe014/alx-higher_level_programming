cd6e06ec731efc00a8e3109a30d7beb624fedeae
Dawit Tamiru <dawittamiru014@gmail.com>
2024-03-21 21:13:45 +0300

    updated

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
-""" 
-    script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa
-    where name matches the argument. But this time, write one that is safe from MySQL injections!
+"""
+Script to display all values in the states table of hbtn_0e_0_usa
+where name matches the argument, safely handling MySQL injections.
 """
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+def main():
+    """
+    Main function that connects to MySQL database and retrieves data.
+    """
+    if len(sys.argv) != 5:
+        print("Usage: ./safe_script.py <username> <password> <database_name> <state_name>")
+        sys.exit(1)
 
+    try:
+        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        c = db.cursor()
+        query = "SELECT * FROM `states` WHERE name=%s"
+        c.execute(query, (sys.argv[4],))
+        for state in c.fetchall():
+            print(state)
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
