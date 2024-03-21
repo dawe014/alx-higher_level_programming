cbb87d5b70a4652c28cf33564edba1b5d615c0ca
Dawit Tamiru <dawittamiru014@gmail.com>
2024-03-21 21:19:13 +0300

    updated

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 """
-Script that takes an argument and displays all values in the states table
-of hbtn_0e_0_usa where the name matches the argument.
+    script that takes in an argument and displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument
 """
-import sys
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: ./script.py <username> <password> <database_name> <state_name>")
-        sys.exit(1)
-
-    try:
-        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-        cursor = db.cursor()
-        query = "SELECT * FROM `states` WHERE BINARY `name` = %s"
-        cursor.execute(query, (sys.argv[4],))
-        for state in cursor.fetchall():
-            print(state)
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        sys.exit(1)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
 """
-    script that lists all State objects from the database hbtn_0e_6_usa
+Script that lists all State objects from the database hbtn_0e_6_usa.
 """
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
+def main():
+    """
+    Main function to list all State objects from the database.
+    
+    Connects to the database using the provided credentials,
+    queries all State objects ordered by id, and prints their id and name.
+    """
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <username> <password> <database_name>")
+        sys.exit(1)
+
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        for instance in session.query(State).order_by(State.id):
+            print(instance.id, instance.name, sep=": ")
+        session.close()
+    except Exception as e:
+        print("An error occurred:", e)
+        sys.exit(1)
+
+
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+    main()
