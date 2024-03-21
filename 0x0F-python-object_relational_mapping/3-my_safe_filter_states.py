6aef20223e9ebb9724bc408daa90e80b453b0d0d
Dawit Tamiru <dawittamiru014@gmail.com>
2024-03-21 21:05:28 +0300

    update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,7 +3,7 @@
     script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa
     where name matches the argument. But this time, write one that is safe from MySQL injections!
 """
-mport sys
+import sys
 import MySQLdb
 
 if __name__ == "__main__":
