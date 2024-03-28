7e63a891b45e3550e48b125439e2f7dfc4ed274f
Dawit Tamiru <dawittamiru014@gmail.com>
2024-03-28 23:02:37 +0300

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # This script retrieves the byte size of the HTTP response header for a specified URL.
-# It utilizes the curl command to fetch the content of the URL silently ("-s") and
-# then pipes it to wc (word count) command with the "-c" option to count the bytes.
+# It uses the curl command to fetch the content of the URL silently ("-s") and
+# then pipes it to the wc (word count) command with the "-c" option to count the bytes.
+
+# Check if an argument (URL) is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Fetch the content of the URL using curl, then count the bytes
 curl -s "$1" | wc -c
