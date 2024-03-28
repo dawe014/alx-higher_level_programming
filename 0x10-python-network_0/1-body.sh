cf99642fdb84b2f42813b6e812e31ec80984f657
Dawit Tamiru <dawittamiru014@gmail.com>
2024-03-28 23:14:09 +0300

    updated

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script retrieves the response body from a specified URL, specifically targeting responses with a status code of 200.
-# It uses the curl command with the options "-sL" to fetch the content of the URL silently and follow redirects if any.
+# takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL "$1"
