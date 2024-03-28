b49b08da5521fff0150bd0b0af51cee2f08b7101
Dawit Tamiru <dawittamiru014@gmail.com>
2024-03-28 23:16:24 +0300

    updated

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a DELETE request to the specified URL and displays the response body.
-# It uses the curl command with the option "-sX DELETE" to send a DELETE request silently ("-s") to the provided URL.
+# sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
