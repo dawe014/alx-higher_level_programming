d98a62c49b9786beb95fcba3740141b92e0cb43c
dawe014 <dawittamiru014@gmail.com>
2024-04-03 00:57:32 +0300

    updated

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -2,4 +2,4 @@
 # This script displays all the HTTP methods that the server of a specified URL will accept.
 # It uses the curl command with the option "-sI" to fetch the HTTP headers of the URL silently, then pipes the output to grep to find the line containing "Allow",
 # and finally, it utilizes cut command to extract and display the HTTP methods from the line by specifying the delimiter "-d " " and the fields "-f 2-".
-curl -s -I "${1}" | grep "^Allow: .*" | cut -d " " -f 2-
+curl -s -X OPTIONS -i "$1" | grep -i "allow:" | sed 's/Allow://i'
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script sends a GET request to the specified URL with a custom header variable.
 # It utilizes the curl command with the option "-sH" to include a custom header "-H" with the value 
-curl -sH "X-School-User-Id: 98" "${1}"
+curl -X GET -H "X-School-User-Id: 98" "${1}"
