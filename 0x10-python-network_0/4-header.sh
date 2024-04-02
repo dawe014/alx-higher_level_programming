80f0f323f65d98146df537dfe3b4d7e0946e262f
dawe014 <dawittamiru014@gmail.com>
2024-04-03 01:34:31 +0300

    updated

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to the specified URL with a custom header variable.
-curl -X GET -H "X-School-User-Id: 98" "${1}"
+curl -sH "X-School-User-Id: 98" "${1}"
